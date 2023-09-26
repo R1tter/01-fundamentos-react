b072205f91f0416e65287224266ed926718ab8da
r1tter <marcelosori@hotmail.com>
2023-09-26 16:07:49 -0300

    feat: post component css style

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -14,15 +14,17 @@ export function Post () {
             <span>Tech Lead</span>
           </div>
         </div>
-
         <time title='26 de Setembro às 14:47' dateTime='2023-09-26 14:47:04'>Publicado há 1h</time>
       </header>
 
       <div className={styles.content}>
         <p>Fala galeraa 👋🏽</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz utilizando o OpenAi. O nome do projeto é UploadAi 🤖</p>
-        <p>👉🏽 <a href="">https://github.com/R1tter/upload-ai</a></p>
-        <p><a href="">#novoprojeto #AI</a></p>
+        <p>👉🏽{' '}<a href="">https://github.com/R1tter/upload-ai</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#AI</a>
+        </p>
       </div>
     </article>
   )
