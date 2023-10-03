7ace3188ecb6f4e5ead03ca40d535b16b4ee8121
r1tter <marcelosori@hotmail.com>
2023-10-03 14:53:54 -0300

    feat: avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from'phosphor-react';
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -10,11 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img 
-          className={styles.avatar}
-          src="https://github.com/R1tter.png" 
-        />
-
+        <Avatar src="https://github.com/R1tter.png" />
         <strong>Marcelo Ritter</strong>
         <span>Technical Leader</span>
       </div>
