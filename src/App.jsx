5cd6678075fc512cdb25857e69af7ba799a60db8
r1tter <marcelosori@hotmail.com>
2023-10-16 11:43:17 -0300

    feat: data mock into posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/R1tter.png',
+      name: 'Marcelo Ritter',
+      role: 'Tech Lead',
+    },
+    content: [
+      {type: 'paragraph', content: 'Hello guys 👋🏽'},
+      {type: 'paragraph', content: 'I just submitted a new project on my github portfolio. It is description generator for videos, using AI to help generate 🤖'},
+      {type: 'link', content: 'github.com/R1tter/upload-ai'},
+    ],
+    publishedAt: new Date('2023-10-16 11:30:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat',
+    },
+    content: [
+      {type: 'paragraph', content: 'Hello guys 👋🏽'},
+      {type: 'paragraph', content: 'I just submitted a new project on my github portfolio. It is description generator for videos, using AI to help generate 🤖'},
+      {type: 'link', content: 'github.com/R1tter/upload-ai'},
+    ],
+    publishedAt: new Date('2023-10-12 11:30:00')
+  },
+]
+
 export function App() {
 
   return (
@@ -15,14 +46,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author = 'John Smith'
-            content = 'Lorem ipsum dolor sit amet, consectetur adip'
-          />
-          <Post
-            author = 'Some other author'
-            content = 'Some other content'
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />  
+            )
+          })}
         </main>
       </div>
     </div>
